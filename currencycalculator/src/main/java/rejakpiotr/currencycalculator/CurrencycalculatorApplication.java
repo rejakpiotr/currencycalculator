02627a52145c8f9bc9b40de488de665652359d2c
Piotr <rejakpiotr@gmail.com>
2021-12-01 21:42:11 +0100

    config commit

---
+++ currencycalculator/src/main/java/rejakpiotr/currencycalculator/CurrencycalculatorApplication.java
@@ -0,0 +1,13 @@
+package rejakpiotr.currencycalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencycalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencycalculatorApplication.class, args);
+	}
+
+}
---
+++ currencycalculator/src/test/java/rejakpiotr/currencycalculator/CurrencycalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package rejakpiotr.currencycalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencycalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
